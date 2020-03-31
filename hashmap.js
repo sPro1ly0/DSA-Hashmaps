56481fbcf45db6ea38590a287db3ba2bc7d9b6cc
Sabrina Becker <sbecka8@gmail.com>
2020-03-31 14:36:36 -0400

    Add initial file with hash map

---
+++ hashmap.js
@@ -0,0 +1,92 @@
+class HashMap {
+    constructor(initialCapacity=8) {
+        this.length = 0;
+        this._hashTable = [];
+        this._capacity = initialCapacity;
+        this._deleted = 0;
+    }
+
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i = 0; i < string.length; i++) {
+            //Bitwise left shift with 5 0s - this would be similar to
+            //hash*31, 31 being the decent prime number
+            //but bit shifting is a faster way to do this
+            //tradeoff is understandability
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            //converting hash to a 32 bit integer
+            hash = hash & hash;
+        }
+        //making sure hash is unsigned - meaning non-negtive number
+        return hash >>> 0;
+    }
+
+    get(key) {
+        const index = this._findSlot(key);
+        if (this._hashTable[index] === undefined) {
+            throw new Error ('Key error');
+        }
+        return this._hashTable[index].value;
+    }
+
+    set(key, value) {
+        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        }
+        // find the slot where this key should be in
+        const index = this._findSlot(key);
+
+        if (!this._hashTable[index]) {
+            this.length++;
+        }
+        this._hashTable[index] = {
+            key,
+            value,
+            DELETED: false
+        };
+    }
+
+    delete(key) {
+        const index = this._findSlot(key);
+        const slot = this._hashTable[index];
+        if (slot === undefined) {
+            throw new Error('Key error');
+        }
+        slot.DELETED = true;
+        this.length--;
+        this._deleted++;
+    }
+    /* The internal private helper function _findSlot() is used 
+    to find the correct slot for a given key. 
+    It uses the private _hashString() function to calculate 
+    the hash of the key, and then uses the modulus to find
+     a slot for the key within the current capacity. */
+    _findSlot(key) {
+        const hash = HashMap._hashString(key); // calculate hash of key
+        const start = hash % this._capacity; // find slot for key within current capacity
+
+        for (let i = start; i < start + this._capacity; i++) {
+            const index = i % this._capacity;
+            const slot = this._hashTable[index];
+            if (slot === undefined || (slot.key === key && !slot.DELETED)) {
+                return index;
+            }
+        }
+    }
+
+    _resize(size) {
+        const oldSlots = this._hashTable;
+        this._capacity = size;
+        // reset the length - it will get rebuilt as you add the items back
+        this._length = 0;
+        this._deleted = 0;
+        this._hashTable = [];
+
+        for (const slot of oldSlots) {
+            if (slot !== undefined && !slot.DELETED) {
+                this.set(slot.key, slot.value);
+            }
+        }
+    }
+}
